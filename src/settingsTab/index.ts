6f11d0058b80ef196ec1b5fa2522917a0a12323b
Hady Osman <hadyos@gmail.com>
2021-05-06 20:07:06 +1200

    Update template variable instructions in settings

--- src/settingsTab/index.ts
+++ src/settingsTab/index.ts
@@ -2,10 +2,11 @@ import { App, PluginSettingTab, Setting } from 'obsidian';
 import pickBy from 'lodash.pickby';
 import { get } from 'svelte/store';
 
-import type KindlePlugin from '.';
-import AmazonLogoutModal from './components/amazonLogoutModal';
-import { settingsStore } from './store';
-import { scrapeLogoutUrl } from './scraper';
+import templateInstructions from './templateInstructions.html';
+import type KindlePlugin from '..';
+import AmazonLogoutModal from '../components/amazonLogoutModal';
+import { settingsStore } from '../store';
+import { scrapeLogoutUrl } from '../scraper';
 
 const { moment } = window;
 
@@ -89,38 +90,16 @@ export class SettingsTab extends PluginSettingTab {
   }
 
   private noteTemplate(): void {
-    const descFragment = document.createRange().createContextualFragment(`
-      Template (<a href="https://mozilla.github.io/nunjucks/">Nunjucks</a>) for rendering every synced Kindle note highlights.
-      <br/><br/>
-      <b>Available variables to use</b>
-      <br/>
-      Book
-      <ul>
-        <li><span class="u-pop">{{title}}</span> - Book title</li>
-        <li><span class="u-pop">{{author}}</span> - Book author</li>
-        <li><span class="u-pop">{{asin}}</span> - Book Amazon Standard Identification Number (ASIN)</li>
-        <li><span class="u-pop">{{url}}</span> - Book url (Amazon)</li>
-        <li><span class="u-pop">{{imageUrl}}</span> - Book cover image</li>
-        <li><span class="u-pop">{{appLink}}</span> - Link to book in local Kindle app</li>
-        <li><span class="u-pop">{{highlights}}</span> - List of your Kindle highlights for this book</li>
-      </ul>
-      <br/>
-      Highlight
-      <ul>
-        <li><span class="u-pop">{{text}}</span> - Highlight text</li>
-        <li><span class="u-pop">{{location}}</span> - Highlight location in book</li>
-        <li><span class="u-pop">{{page}}</span> - Highlighted page location in book</li>
-        <li><span class="u-pop">{{note}}</span> - Your note associated with highlight</li>
-        <li><span class="u-pop">{{appLink}}</span> - Link to highlighted text in local Kindle app</li>
-      </ul>
-    `);
+    const descFragment = document
+      .createRange()
+      .createContextualFragment(templateInstructions);
 
     new Setting(this.containerEl)
       .setName('Note template')
       .setDesc(descFragment)
       .addTextArea((text) => {
         text.inputEl.style.width = '100%';
-        text.inputEl.style.height = '250px';
+        text.inputEl.style.height = '450px';
         text.inputEl.style.fontSize = '0.8em';
         text
           .setValue(get(settingsStore).noteTemplate)
